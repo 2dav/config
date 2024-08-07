3031330c4709a378f92e944e5e225151affb705d
2dav <2dav@pm.me>
2024-08-07 19:00:02 +0300

    Reformat

--- hosts/packages.nix
+++ hosts/packages.nix
@@ -1,57 +1,60 @@
 {pkgs, ...}: with pkgs; [
   busybox
-  neovim nano tmux
-  emacs 
-  emacs-all-the-icons-fonts
   nix-index
   uutils-coreutils wget rsync
-  telegram-desktop
-  discord
   openssh
   git git-extras git-lfs
-  zsh oh-my-zsh
-  firefox
-  ungoogled-chromium
-  alacritty kitty
-  htop
   unrar unzip
-  ripgrep
-  fd
-  xdg-user-dirs
-  grim
 
-  rustup
-  valgrind
-  ghidra
-  
-  dunst
   libnotify
   libinput
   libwacom
-  pavucontrol
-  pulseaudio
-  wl-clipboard
-  copyq
-  fish
+
   eza
-  clang
-  clang-tools
-  zig
   fzf
   wine64
   pandoc
+  ripgrep
+  fd
+  htop
 
+  grim
+  dunst
+  pavucontrol
+  pulseaudio
+  copyq
+  zsh oh-my-zsh
+  fish
+  alacritty kitty
+
+  valgrind
+  ghidra
+  clang
+  clang-tools
+
+  rustup
+  zig
+
+  telegram-desktop
+  discord
+  firefox
+  ungoogled-chromium
+
+  neovim nano tmux
+  emacs 
+  emacs-all-the-icons-fonts
+
+  xdg-user-dirs
   (aspellWithDicts
           (dicts: with dicts; [ en en-computers en-science es ru ]))
   shellcheck
-
   (rofi-wayland.override {
     plugins = [ pkgs.rofi-calc ];
   })
   yambar
   hyprpaper
   waylock
-
+  wl-clipboard
   (callPackage ./pkgs/yambar-hyprland-wses-alpha.nix {})
   #yambar-hyprland-wses
 
