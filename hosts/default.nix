752ed71eb8ef759fead14d47f97fb1bc0a1def56
2dav <2davy@pm.me>
2024-09-10 11:43:54 +0300

    fixes sound glitches thr HDMI

--- hosts/default.nix
+++ hosts/default.nix
@@ -69,10 +69,10 @@
 	};
     pipewire = {
       enable = true;
-      pulse.enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      jack.enable = true;
+	  audio.enable = true;
+	  pulse.enable = true;
+	  alsa.enable = true;
+	  wireplumber.enable = true;
     };
     greetd = {
       enable = true;
